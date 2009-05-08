b4b5f9de7d366e09c5b0a369bd20903a0e7660ee
vvoody <wxj@vvoody.org>
2009-05-08 20:44:17 +0800

    First commit & create cmprurl.py

---
+++ cmprzurl.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+import cgi
+import cgitb; cgitb.enable()
+
+def check_url(aurl):
+    """
+    Check whether the url is valid or not.
+    """
+    return True
+
+form = cgi.FieldStorage()
+
+if form.has_key('longurl') and check_url( form['longurl'].value ):
+    print "Content-Type: text/html\n"
+    #
+    # generate unique string for mapping longurl &
+    # connect the db and write into it
+    import random
+    from pysqlite2 import dbapi2 as sqlite3
+    #
+    con = sqlite3.connect('cmprzurl.db')
+    con.isolation_level = None
+    cur = con.cursor()
+    # ...
+    randseed = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    goon = True
+    while goon:
+        # generate a random string for mapping the longurl
+        shorterurl = ""
+        for i in range(6):
+            shorterurl += random.choice(randseed)
+        #
+        # try write 'shorterurl' and 'longurl' into db:
+        # ...
+        # except: used map string or same url
+        #     del shorterurl
+        #     continue
+	print shorterurl
+	try:
+	    inskey = """insert into mapurl values('%s', NULL);""" % shorterurl
+	    cur.execute(inskey)
+	except:
+	    del shorterurl
+	    continue
+	try:
+	    inslongurl = """update mapurl set longurl='%s' where key='%s';""" % (form['longurl'].value, shorterurl)
+	    cur.execute(inslongurl)
+	except: # longurl is not unique, means that it had been shorted ;-)
+	    # print """select key from mapurl where longurl=form['longurl'].value"""
+	    print "This url had been shorted ;-)"
+	    break
+        goon = False
+    #
+    cur.close()
+    con.close()
+    # no except, genrate htmls. end here
+else:
+    print "Content-Type: text/html\n"
+    print "Ooooops... Check you url!"
